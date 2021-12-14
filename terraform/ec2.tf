6803577725f321947f41193ee1c7ce5c6318c29a
Robin Schrievers <robin.schrievers@dtn.comm>
2021-12-14 12:52:36 +0100

    :mag: Cleaning up some more

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -18,22 +18,3 @@ resource "aws_security_group" "webapp_ec2" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-# # EC2 instances, one per availability zone
-# resource "aws_instance" "webapp" {
-#   ami                         = local.ami
-#   associate_public_ip_address = true
-#   #count                       = "${length(var.azs)}"
-#   //depends_on                  = ["aws_subnet.private"]
-#   instance_type               = "t2.micro"
-#   subnet_id                   = element(local.subnets, 0)
-#   user_data                   = file("userdata.sh")
-
-#   # references security group created above
-#   vpc_security_group_ids = [aws_security_group.webapp_ec2.id]
-
-#   tags = {
-#     Name = "webapp-instance"//-${count.index}"
-#     Environment = var.environment
-#   }
-# }
